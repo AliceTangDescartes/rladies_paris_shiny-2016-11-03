dcfc1bb861cfc260193efef564b7faff8f4613df
Page Piccinini <page.piccinini@gmail.com>
2016-10-19 09:42:24 +0200

    Add other election data from Wikipedia.

--- app.R
+++ app.R
@@ -5,7 +5,12 @@ library(shiny)
 ## MAKE UI INPUTS ####
 ui <- fluidPage(
   selectInput(inputId = "year", label = "Election Year",
-              choices = c("1964", "1972", "1980", "1984", "1992", "1996", "2004", "2012")),
+              choices = c("1789", "1792", "1796", "1800", "1804", "1808", "1812", "1816", "1820", "1824", "1828",
+                            "1832", "1836", "1840", "1844", "1848", "1852", "1856", "1860", "1864", "1868", "1872",
+                            "1876", "1880", "1884", "1888", "1892", "1896", "1900", "1904", "1908", "1912", "1916",
+                            "1920", "1924", "1928", "1932", "1936", "1940", "1944", "1948", "1952", "1956", "1960",
+                            "1964", "1968", "1972", "1976", "1980", "1984", "1988", "1992", "1996", "2000", "2004",
+                            "2008", "2012")),
   
   plotOutput("map")
 )
@@ -13,13 +18,13 @@ ui <- fluidPage(
 
 ## MAKE SERVER OUTPUTS
 server <- function(input, output) {
-  library(dplyr)
-  library(ggplot2)
+  library(tidyverse)
   library(maps)
   library(mapproj)
   
-  data = read.table("data/data.txt", header=T, sep="\t") %>%
-     mutate(year = factor(year))
+  data = read.table("data/data_us_presidential_elections.txt", header=T, sep="\t") %>%
+    mutate(year = factor(year)) %>%
+    mutate(state = tolower(state))
 
   states = map_data("state") %>%
     rename(state = region)
@@ -29,7 +34,7 @@ server <- function(input, output) {
   output$map = renderPlot({
     filter(data_plot, year == input$year) %>%
     ggplot() +
-      geom_polygon(aes(x=long, y=lat, group = group, fill = winner),
+      geom_polygon(aes(x=long, y=lat, group = group, fill = party_winner),
                     colour="white") +
       scale_fill_manual(values = c("blue", "red")) +
       coord_map(projection = "polyconic") +
